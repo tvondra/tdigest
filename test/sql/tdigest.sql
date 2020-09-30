3d6ed209c0741f98792766e4b01e58e13c80ccbf
Tomas Vondra <tv@fuzzy.cz>
2020-09-30 22:49:33 +0200

    Bump version to 1.0.1

--- test/sql/tdigest.sql
+++ test/sql/tdigest.sql
@@ -3,6 +3,7 @@
 -- disable the notices for the create script (shell types etc.)
 SET client_min_messages = 'WARNING';
 \i tdigest--1.0.0.sql
+\i tdigest--1.0.0--1.0.1.sql
 SET client_min_messages = 'NOTICE';
 
 \set ECHO all
