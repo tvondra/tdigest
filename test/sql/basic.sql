d050cc9004961851442b232b6ffe0a1d337167f7
Nils Dijk <nils@citusdata.com>
2023-09-25 12:58:45 +0200

    Stamp 1.4.1 (#30)

--- test/sql/basic.sql
+++ test/sql/basic.sql
@@ -7,6 +7,7 @@ SET client_min_messages = 'WARNING';
 \i tdigest--1.0.1--1.2.0.sql
 \i tdigest--1.2.0--1.3.0.sql
 \i tdigest--1.3.0--1.4.0.sql
+\i tdigest--1.4.0--1.4.1.sql
 SET client_min_messages = 'NOTICE';
 SET extra_float_digits = 0;
 
