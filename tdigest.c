tvondra/tdigest

commit 7e552073e4299c35c885c278a6e784c3f97902ef

Add cast from tdigest to JSON
This commit allows formatting t-digest values as valid JSON documents,
making it easier to use them in JavaScript apps. The default text format
was meant to be human readable, and would require parsing and building
the JSON manually.

We make sure to always print mean, even for tdigests in the older format
storing sum for centroids. Otherwise the "mean" key would be confusing.
But we don't call tdigest_update_format, and instead we simply update
the flags and convert the sum/mean values.

The centroids are stored in two separate arrays - one for means, one for
counts. That makes it easier to process, because it's clear the i-th in
each array is for i-th centroid. We might store it in a single array,
but then we'd have to walk it in pairs. And it'd mix float and int
values in the same array.

tdigest.c
@@ -15,6 +15,7 @@
 #include "postgres.h"
 #include "libpq/pqformat.h"
 #include "utils/array.h"
+#include "utils/builtins.h"
 #include "utils/lsyscache.h"
 #include "catalog/pg_type.h"
 
@@ -135,6 +136,7 @@ PG_FUNCTION_INFO_V1(tdigest_send);
 PG_FUNCTION_INFO_V1(tdigest_recv);
 
 PG_FUNCTION_INFO_V1(tdigest_count);
+PG_FUNCTION_INFO_V1(tdigest_to_json);
 
 PG_FUNCTION_INFO_V1(tdigest_add_double_increment);
 PG_FUNCTION_INFO_V1(tdigest_add_double_array_increment);
@@ -175,6 +177,8 @@ Datum tdigest_count(PG_FUNCTION_ARGS);
 Datum tdigest_add_double_increment(PG_FUNCTION_ARGS);
 Datum tdigest_add_double_array_increment(PG_FUNCTION_ARGS);
 Datum tdigest_union_double_increment(PG_FUNCTION_ARGS);
+
+Datum tdigest_to_json(PG_FUNCTION_ARGS);
 
 static Datum double_to_array(FunctionCallInfo fcinfo, double * d, int len);
 static double *array_to_double(FunctionCallInfo fcinfo, ArrayType *v, int * len);
@@ -2709,6 +2713,80 @@ tdigest_count(PG_FUNCTION_ARGS)
 	tdigest_t  *digest = (tdigest_t *) PG_DETOAST_DATUM(PG_GETARG_DATUM(0));
 
 	PG_RETURN_INT64(digest->count);
+}
+
+/*
+ * tdigest_to_json
+ *		Transform the tdigest into a JSON value.
+ *
+ * We make sure to always print mean, even for tdigests in the older format
+ * storing sum for centroids. Otherwise the "mean" key would be confusing.
+ * But we don't call tdigest_update_format, and instead we simply update the
+ * flags and convert the sum/mean values.
+ *
+ * The centroids are stored in two separate arrays - one for means, one for
+ * counts. That makes it easier to process, because it's clear the i-th
+ * in each array is for i-th centroid. We might store it in a single array,
+ * but then we'd have to walk it in pairs. And it'd mix float and int
+ * values in the same array.
+ */
+Datum
+tdigest_to_json(PG_FUNCTION_ARGS)
+{
+	int				i;
+	StringInfoData	str;
+	tdigest_t	   *digest = (tdigest_t *) PG_DETOAST_DATUM(PG_GETARG_DATUM(0));
+	int32			flags = digest->flags;
+
+	initStringInfo(&str);
+
+	appendStringInfoChar(&str, '{');
+
+	flags |= TDIGEST_STORES_MEAN;
+
+	appendStringInfo(&str, "\"flags\": %d, ", flags);
+	appendStringInfo(&str, "\"count\": " INT64_FORMAT ", ", digest->count);
+	appendStringInfo(&str, "\"compression\": %d, ", digest->compression);
+	appendStringInfo(&str, "\"centroids\": %d, ", digest->ncentroids);
+
+	appendStringInfoString(&str, "\"mean\": [");
+
+	for (i = 0; i < digest->ncentroids; i++)
+	{
+		double	mean = digest->centroids[i].mean;
+
+		if (i > 0)
+			appendStringInfoString(&str, ", ");
+
+		/*
+		 * When the TDIGEST_STORES_MEAN flags is not set, the value is
+		 * actually a sum, so convert it to mean now. We have to check the
+		 * diget->flags, not the local variable.
+		 */
+		if (! (digest->flags && TDIGEST_STORES_MEAN))
+			mean = mean / digest->centroids[i].count;
+
+		/* don't print insignificant zeroes to the right of decimal point */
+		appendStringInfo(&str, "%g", mean);
+	}
+
+	appendStringInfoString(&str, "], ");
+
+	appendStringInfoString(&str, "\"count\": [");
+
+	for (i = 0; i < digest->ncentroids; i++)
+	{
+		if (i > 0)
+			appendStringInfoString(&str, ", ");
+
+		appendStringInfo(&str, INT64_FORMAT, digest->centroids[i].count);
+	}
+
+	appendStringInfoString(&str, "]");
+
+	appendStringInfoChar(&str, '}');
+
+	PG_RETURN_TEXT_P(cstring_to_text(str.data));
 }
 
 /*

Makefile
@@ -2,7 +2,7 @@ MODULE_big = tdigest
 OBJS = tdigest.o
 
 EXTENSION = tdigest
-DATA = tdigest--1.0.0.sql tdigest--1.0.0--1.0.1.sql tdigest--1.0.1--1.2.0.sql
+DATA = tdigest--1.0.0.sql tdigest--1.0.0--1.0.1.sql tdigest--1.0.1--1.2.0.sql  tdigest--1.2.0--1.3.0-dev.sql
 MODULES = tdigest
 
 CFLAGS=`pg_config --includedir-server`

tdigest--1.2.0--1.3.0-dev.sql
@@ -1,0 +1,8 @@
+CREATE FUNCTION tdigest_json(tdigest)
+    RETURNS json
+    AS 'tdigest', 'tdigest_to_json'
+    LANGUAGE C IMMUTABLE STRICT;
+
+CREATE CAST (tdigest AS json)
+    WITH FUNCTION tdigest_json(tdigest)
+    AS ASSIGNMENT;

test/expected/tdigest.out
@@ -1297,6 +1297,25 @@ SELECT * FROM (
 ---+----+----
 (0 rows)
 
+-- test casting to json
+SELECT cast(tdigest(i / 10000.0, 10) as json) from generate_series(1,10000) s(i);
+                                                                                                                               tdigest                                                                                                                               
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ {"flags": 1, "count": 10000, "compression": 10, "centroids": 14, "mean": [0.0001, 0.0004, 0.0013, 0.00405, 0.0135, 0.05375, 0.2351, 0.59225, 0.88255, 0.9768, 0.99275, 0.99775, 0.9997, 1], "count": [1, 5, 13, 42, 147, 658, 2969, 4174, 1632, 253, 66, 34, 5, 1]}
+(1 row)
+
+SELECT cast(tdigest(i / 10000.0, 25) as json) from generate_series(1,10000) s(i);
+                                                                                                                                                                                     tdigest                                                                                                                                                                                      
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ {"flags": 1, "count": 10000, "compression": 25, "centroids": 23, "mean": [0.0001, 0.00025, 0.00055, 0.0012, 0.00265, 0.00575, 0.0115, 0.0232, 0.0472, 0.086, 0.16815, 0.344, 0.5525, 0.7565, 0.902, 0.95005, 0.9761, 0.9919, 0.99755, 0.99925, 0.99975, 0.9999, 1], "count": [1, 2, 4, 9, 20, 42, 73, 161, 319, 457, 1186, 2331, 1839, 2241, 669, 292, 229, 87, 26, 8, 2, 1, 1]}
+(1 row)
+
+SELECT cast(tdigest(i / 10000.0, 100) as json) from generate_series(1,10000) s(i);
+                                                                                                                                                                                                                                                                                                                                                                         tdigest                                                                                                                                                                                                                                                                                                                                                                          
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ {"flags": 1, "count": 10000, "compression": 100, "centroids": 52, "mean": [0.0001, 0.0002, 0.0003, 0.0004, 0.00055, 0.00075, 0.001, 0.00135, 0.0018, 0.00245, 0.0034, 0.0047, 0.0065, 0.009, 0.01245, 0.0171, 0.02315, 0.03075, 0.03985, 0.0501, 0.06455, 0.0842, 0.10595, 0.13495, 0.18065, 0.24805, 0.33, 0.42235, 0.5092, 0.60375, 0.6915, 0.75715, 0.8183, 0.8637, 0.8966, 0.91835, 0.93985, 0.96045, 0.9739, 0.98335, 0.9894, 0.99325, 0.9957, 0.99725, 0.99825, 0.9989, 0.9993, 0.99955, 0.9997, 0.9998, 0.9999, 1], "count": [1, 1, 1, 1, 2, 2, 3, 4, 5, 8, 11, 15, 21, 29, 40, 53, 68, 84, 98, 107, 182, 211, 224, 356, 558, 790, 849, 998, 739, 1152, 603, 710, 513, 395, 263, 172, 258, 154, 115, 74, 47, 30, 19, 12, 8, 5, 3, 2, 1, 1, 1, 1]}
+(1 row)
+
 -- <value,count> API
 select tdigest_percentile(value, count, 100, 0.95)
 from (values
@@ -1498,4 +1517,5 @@ SELECT (SELECT tdigest(d)::text FROM t) = (SELECT tdigest(x.d)::text FROM x);
 ----------
  t
 (1 row)
+
 

test/sql/tdigest.sql
@@ -5,6 +5,7 @@ SET client_min_messages = 'WARNING';
 \i tdigest--1.0.0.sql
 \i tdigest--1.0.0--1.0.1.sql
 \i tdigest--1.0.1--1.2.0.sql
+\i tdigest--1.2.0--1.3.0-dev.sql
 SET client_min_messages = 'NOTICE';
 
 \set ECHO all
@@ -971,6 +972,11 @@ SELECT * FROM (
         GROUP BY perc.percentiles
     ) foo
 ) bar where v2 > v1;
+
+-- test casting to json
+SELECT cast(tdigest(i / 10000.0, 10) as json) from generate_series(1,10000) s(i);
+SELECT cast(tdigest(i / 10000.0, 25) as json) from generate_series(1,10000) s(i);
+SELECT cast(tdigest(i / 10000.0, 100) as json) from generate_series(1,10000) s(i);
 
 -- <value,count> API
 

commit 23e731990888e1bdc8081a04062fe669b111db25

Avoid duplicate keys in the JSON representation of a digest
tdigest_json() used the "count" key both for the total number of items
and for the array of per-centroid counts. Duplicate keys are not
forbidden by the JSON grammar, but hardly any parser preserves both
values. The jsonb type keeps the last occurrence, so casting to jsonb
silently replaced the total count with the array.

Rename the per-centroid arrays to "means" and "counts", so that parsing
the document does not lose the total count. Using plural names for the
arrays keeps the scalar keys ("flags", "count", "compression" and
"centroids") consistent with the text representation, and describes the
contents a bit better too.

In principle, this is a breaking change. However, it's hard to believe
anyone is using the JSON value without running into the duplicate key.

The issue exists since version 1.2.0, which introduced the JSON format
in commit 7e552073.

tdigest.c
@@ -3978,6 +3978,11 @@ tdigest_count(PG_FUNCTION_ARGS)
  * in each array is for i-th centroid. We might store it in a single array,
  * but then we'd have to walk it in pairs. And it'd mix float and int
  * values in the same array.
+ *
+ * The arrays are named in plural ("means" and "counts"), so that the array
+ * of per-centroid counts does not collide with the total count. Duplicate
+ * keys are not strictly forbidden by JSON, but most parsers keep just one
+ * of the values (jsonb keeps the last one), losing the other.
  */
 Datum
 tdigest_to_json(PG_FUNCTION_ARGS)
@@ -3998,7 +4003,7 @@ tdigest_to_json(PG_FUNCTION_ARGS)
 	appendStringInfo(&str, "\"compression\": %d, ", digest->compression);
 	appendStringInfo(&str, "\"centroids\": %d, ", digest->ncentroids);
 
-	appendStringInfoString(&str, "\"mean\": [");
+	appendStringInfoString(&str, "\"means\": [");
 
 	for (i = 0; i < digest->ncentroids; i++)
 	{
@@ -4027,7 +4032,7 @@ tdigest_to_json(PG_FUNCTION_ARGS)
 
 	appendStringInfoString(&str, "], ");
 
-	appendStringInfoString(&str, "\"count\": [");
+	appendStringInfoString(&str, "\"counts\": [");
 
 	for (i = 0; i < digest->ncentroids; i++)
 	{

test/expected/cast.out
@@ -1,20 +1,20 @@
 -- test casting to json
 SELECT cast(tdigest(i / 1000.0, 10) as json) from generate_series(1,1000) s(i);
-                                                                                                                                                    tdigest                                                                                                                                                     
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- {"flags": 1, "count": 1000, "compression": 10, "centroids": 13, "mean": [0.001, 0.002, 0.0045, 0.012999999999999998, 0.0405, 0.135, 0.4640000000000002, 0.7929999999999998, 0.9159999999999996, 0.9794999999999999, 0.9959999999999999, 0.999, 1], "count": [1, 1, 4, 13, 42, 147, 511, 147, 99, 28, 5, 1, 1]}
+                                                                                                                                                     tdigest                                                                                                                                                      
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ {"flags": 1, "count": 1000, "compression": 10, "centroids": 13, "means": [0.001, 0.002, 0.0045, 0.012999999999999998, 0.0405, 0.135, 0.4640000000000002, 0.7929999999999998, 0.9159999999999996, 0.9794999999999999, 0.9959999999999999, 0.999, 1], "counts": [1, 1, 4, 13, 42, 147, 511, 147, 99, 28, 5, 1, 1]}
 (1 row)
 
 SELECT cast(tdigest(i / 1000.0, 25) as json) from generate_series(1,1000) s(i);
-                                                                                                                                                                                              tdigest                                                                                                                                                                                               
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- {"flags": 1, "count": 1000, "compression": 25, "centroids": 18, "mean": [0.001, 0.002, 0.003, 0.0055000000000000005, 0.011999999999999997, 0.0265, 0.05749999999999998, 0.11499999999999992, 0.232, 0.4720000000000001, 0.727, 0.8775, 0.9489999999999998, 0.9764999999999998, 0.9914999999999998, 0.997, 0.999, 1], "count": [1, 1, 1, 4, 9, 20, 42, 73, 161, 319, 191, 110, 33, 22, 8, 3, 1, 1]}
+                                                                                                                                                                                               tdigest                                                                                                                                                                                                
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ {"flags": 1, "count": 1000, "compression": 25, "centroids": 18, "means": [0.001, 0.002, 0.003, 0.0055000000000000005, 0.011999999999999997, 0.0265, 0.05749999999999998, 0.11499999999999992, 0.232, 0.4720000000000001, 0.727, 0.8775, 0.9489999999999998, 0.9764999999999998, 0.9914999999999998, 0.997, 0.999, 1], "counts": [1, 1, 1, 4, 9, 20, 42, 73, 161, 319, 191, 110, 33, 22, 8, 3, 1, 1]}
 (1 row)
 
 SELECT cast(tdigest(i / 1000.0, 100) as json) from generate_series(1,1000) s(i);
-                                                                                                                                                                                                                                                                                                                                                                                                                      tdigest                                                                                                                                                                                                                                                                                                                                                                                                                      
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- {"flags": 1, "count": 1000, "compression": 100, "centroids": 40, "mean": [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.0075, 0.009999999999999998, 0.013499999999999998, 0.018000000000000002, 0.0245, 0.03400000000000001, 0.04699999999999999, 0.065, 0.08999999999999998, 0.12450000000000006, 0.17099999999999987, 0.23149999999999998, 0.30749999999999994, 0.39850000000000024, 0.5010000000000003, 0.6034999999999998, 0.6944999999999996, 0.7704999999999996, 0.8309999999999995, 0.8774999999999997, 0.9119999999999999, 0.9369999999999997, 0.9550000000000003, 0.968, 0.9775, 0.9840000000000001, 0.9884999999999999, 0.992, 0.9944999999999999, 0.996, 0.997, 0.998, 0.999, 1], "count": [1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 8, 11, 15, 21, 29, 40, 53, 68, 84, 98, 107, 98, 84, 68, 53, 40, 29, 21, 15, 11, 8, 5, 4, 3, 2, 1, 1, 1, 1, 1]}
+                                                                                                                                                                                                                                                                                                                                                                                                                       tdigest                                                                                                                                                                                                                                                                                                                                                                                                                       
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ {"flags": 1, "count": 1000, "compression": 100, "centroids": 40, "means": [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.0075, 0.009999999999999998, 0.013499999999999998, 0.018000000000000002, 0.0245, 0.03400000000000001, 0.04699999999999999, 0.065, 0.08999999999999998, 0.12450000000000006, 0.17099999999999987, 0.23149999999999998, 0.30749999999999994, 0.39850000000000024, 0.5010000000000003, 0.6034999999999998, 0.6944999999999996, 0.7704999999999996, 0.8309999999999995, 0.8774999999999997, 0.9119999999999999, 0.9369999999999997, 0.9550000000000003, 0.968, 0.9775, 0.9840000000000001, 0.9884999999999999, 0.992, 0.9944999999999999, 0.996, 0.997, 0.998, 0.999, 1], "counts": [1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 8, 11, 15, 21, 29, 40, 53, 68, 84, 98, 107, 98, 84, 68, 53, 40, 29, 21, 15, 11, 8, 5, 4, 3, 2, 1, 1, 1, 1, 1]}
 (1 row)
 
 -- test casting to double precision array

test/expected/cast_1.out
@@ -1,20 +1,20 @@
 -- test casting to json
 SELECT cast(tdigest(i / 1000.0, 10) as json) from generate_series(1,1000) s(i);
-                                                                                                             tdigest                                                                                                             
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- {"flags": 1, "count": 1000, "compression": 10, "centroids": 13, "mean": [0.001, 0.002, 0.0045, 0.013, 0.0405, 0.135, 0.464, 0.793, 0.916, 0.9795, 0.996, 0.999, 1], "count": [1, 1, 4, 13, 42, 147, 511, 147, 99, 28, 5, 1, 1]}
+                                                                                                              tdigest                                                                                                              
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ {"flags": 1, "count": 1000, "compression": 10, "centroids": 13, "means": [0.001, 0.002, 0.0045, 0.013, 0.0405, 0.135, 0.464, 0.793, 0.916, 0.9795, 0.996, 0.999, 1], "counts": [1, 1, 4, 13, 42, 147, 511, 147, 99, 28, 5, 1, 1]}
 (1 row)
 
 SELECT cast(tdigest(i / 1000.0, 25) as json) from generate_series(1,1000) s(i);
-                                                                                                                                         tdigest                                                                                                                                         
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- {"flags": 1, "count": 1000, "compression": 25, "centroids": 18, "mean": [0.001, 0.002, 0.003, 0.0055, 0.012, 0.0265, 0.0575, 0.115, 0.232, 0.472, 0.727, 0.8775, 0.949, 0.9765, 0.9915, 0.997, 0.999, 1], "count": [1, 1, 1, 4, 9, 20, 42, 73, 161, 319, 191, 110, 33, 22, 8, 3, 1, 1]}
+                                                                                                                                          tdigest                                                                                                                                          
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ {"flags": 1, "count": 1000, "compression": 25, "centroids": 18, "means": [0.001, 0.002, 0.003, 0.0055, 0.012, 0.0265, 0.0575, 0.115, 0.232, 0.472, 0.727, 0.8775, 0.949, 0.9765, 0.9915, 0.997, 0.999, 1], "counts": [1, 1, 1, 4, 9, 20, 42, 73, 161, 319, 191, 110, 33, 22, 8, 3, 1, 1]}
 (1 row)
 
 SELECT cast(tdigest(i / 1000.0, 100) as json) from generate_series(1,1000) s(i);
-                                                                                                                                                                                                                                                              tdigest                                                                                                                                                                                                                                                              
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- {"flags": 1, "count": 1000, "compression": 100, "centroids": 40, "mean": [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.0075, 0.01, 0.0135, 0.018, 0.0245, 0.034, 0.047, 0.065, 0.09, 0.1245, 0.171, 0.2315, 0.3075, 0.3985, 0.501, 0.6035, 0.6945, 0.7705, 0.831, 0.8775, 0.912, 0.937, 0.955, 0.968, 0.9775, 0.984, 0.9885, 0.992, 0.9945, 0.996, 0.997, 0.998, 0.999, 1], "count": [1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 8, 11, 15, 21, 29, 40, 53, 68, 84, 98, 107, 98, 84, 68, 53, 40, 29, 21, 15, 11, 8, 5, 4, 3, 2, 1, 1, 1, 1, 1]}
+                                                                                                                                                                                                                                                               tdigest                                                                                                                                                                                                                                                               
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ {"flags": 1, "count": 1000, "compression": 100, "centroids": 40, "means": [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.0075, 0.01, 0.0135, 0.018, 0.0245, 0.034, 0.047, 0.065, 0.09, 0.1245, 0.171, 0.2315, 0.3075, 0.3985, 0.501, 0.6035, 0.6945, 0.7705, 0.831, 0.8775, 0.912, 0.937, 0.955, 0.968, 0.9775, 0.984, 0.9885, 0.992, 0.9945, 0.996, 0.997, 0.998, 0.999, 1], "counts": [1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 8, 11, 15, 21, 29, 40, 53, 68, 84, 98, 107, 98, 84, 68, 53, 40, 29, 21, 15, 11, 8, 5, 4, 3, 2, 1, 1, 1, 1, 1]}
 (1 row)
 
 -- test casting to double precision array